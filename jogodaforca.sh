52f2a3578b85d3ec2a5743f6b853120232658f3a
handyman0 <luiz.number@gmail.com>
2023-11-18 15:47:35 -0300

    finalizado

--- jogodaforca.sh
+++ jogodaforca.sh
@@ -121,7 +121,7 @@ clear
 echo "jogo da forca"
 layout
 # palavra para sortear
-PALAVRA="garrafa"
+PALAVRA=$(shuf -n 1 palavras.txt)
 
 # fazer um verificador de numeros de tentativas
 NUM_TENTATIVAS=$(echo -n "$PALAVRA" | fold -w1 | sort -u | wc -l)
@@ -132,7 +132,7 @@ TENTATIVAS_ERRADAS=()
 
 while true; do
 	# solicitando ao jogandor uma letra
-	read -p "to pensando em uma palavra!, chute uma letra: " LETRA
+	read -p "to pensando em um animal!, chute uma letra: " LETRA
 	
 	# fazendo o tratamento de minusculas e maiusculas
 	LETRA=$(echo $LETRA | tr '[:upper:]' '[:lower:]')
@@ -171,8 +171,9 @@ while true; do
 		fi
 	else
 		clear
-		echo "você errou, chute mais uma letra" 
-		echo "tem 7 letras!"
+		echo "você errou, chute mais uma letra"
+		QUANLETRAS=$(echo -n $PALAVRA | wc -c) 
+		echo "tem $QUANLETRAS letras!"
 		TENTATIVAS_ERRADAS+=("$LETRA")
 		
 		# Mostrar as partes correspondentes do boneco
@@ -197,6 +198,11 @@ while true; do
 				;;
 		esac
 	fi
+	if [[ "${LETRA^^}" == "DESISTO" ]]; then
+		echo "Você desistiu! A palavra era: $PALAVRA"
+		break
+	fi
+ 
 
 	# fazendo uma lista de letras certas e erradas
 	echo "tentativas certas: ${TENTATIVAS_CERTAS[*]}"
@@ -207,19 +213,31 @@ while true; do
 	# verificando se o jogo acabou
 	if (( ${#TENTATIVAS_ERRADAS[@]} >= NUM_TENTATIVAS )); then
 		echo "Você excedeu o número de tentativas permitidas. Fim de jogo!"
+		echo "a palavra misteriosa era: $PALAVRA"
 		break
 	fi
 	if (( ${#TENTATIVAS_CERTAS[@]} >= $(echo -n "$PALAVRA" | fold -w1 | sort -u | wc -l) )); then
-		read -p "você colocou todas as letras! chute a palavra:" CHUTE
-		if [ "$CHUTE" == "$PALAVRA" ]; then
-			clear
-			echo "PARABÉNS, VOCÊ ACERTOU!"
-			echo "a palavra misteriosa é $PALAVRA"
-			sleep 3
-			break
-		else
-			echo "você errou!"
-		fi
+		palavra_completa=1
+    
+		for letra in $(echo -n "$PALAVRA" | fold -w1 | sort -u); do
+			if [[ ! " ${TENTATIVAS_CERTAS[*]} " =~ " $letra " ]]; then
+				palavra_completa=0
+				break
+			fi
+		done
+
+		if (( palavra_completa == 1 )); then
+        		read -p "Você colocou todas as letras! Chute a palavra:" CHUTE
+        		if [[ "${CHUTE,,}" == "$PALAVRA" ]]; then
+            			clear
+            			echo "PARABÉNS, VOCÊ ACERTOU!"
+            			echo "A palavra misteriosa é $PALAVRA"
+            			sleep 3
+            			break
+			else
+				echo "Você errou ao tentar adivinhar a palavra!"
+        		fi
+    		fi
 	fi
 done
 
